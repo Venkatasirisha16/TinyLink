19db0209d0dc4ebae6cc5a374a9192865cb18f64
sirisha bommuluri <sirisha.b@inovar-tech.com>
2025-11-22 21:24:40 +0530

    Added the cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import redirectRoute from "./routes/redirectRoute.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/links", linkRoutes);
